e4ceb1237d2538eb751bdf5bf712f88c849cc852
AdrianMora8 <adrianmora8180@gmail.com>
2024-01-25 23:33:35 -0500

    diseño-login

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,5 +1,36 @@
+import './Login.css';
+
 export const Login = () => {
     return (
-        <div>Login</div>
+        <div className='container'>
+            <div className='content'>
+                <div className='input'>
+
+                    <input 
+                    type='text' 
+                    placeholder='Usuario'
+                    >
+                    </input>
+                    
+
+                    <input
+                    type='text'
+                    placeholder='contraseña'
+                    >
+                    </input>
+
+                    <button>
+                        <span >
+                            Login
+                        </span>
+                    </button>
+
+                    
+
+                </div>
+            </div>
+
+
+        </div>
     )
 }
